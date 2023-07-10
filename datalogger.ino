9a61f27ea63dccc6323cba6aea3477bf38b58c37
hofmn <hofmanndavid92@gmail.com>
2023-07-10 11:32:50 +0200

    RTC hinzugefügt

--- datalogger.ino
+++ datalogger.ino
@@ -3,12 +3,18 @@
 #include "FS.h"
 #include "SD.h"
 #include "SPI.h"
+#include <Wire.h>
+#include <RTClib.h>
 
 
 #define dataloggerID "/left.txt"
 #define dhtPin 17
 #define dhtType DHT22
+#define rtcSDA 26
+#define rtcSCL 18
 DHT dht(dhtPin, dhtType);
+RTC_DS3231 rtc;
+
 void appendFile(fs::FS &fs, const char * path, const char * message){
   Serial.printf("Appending to file: %s\n", path);
 
@@ -28,14 +34,24 @@ void appendFile(fs::FS &fs, const char * path, const char * message){
 
 void setup(){
   Serial.begin(115200);
-  if(!SD.begin(5)){
-    Serial.println("Card Mount Failed");
-    return;
-  }
-  uint8_t cardType = SD.cardType();
-  Serial.printf("Total space: %lluMB\n", SD.totalBytes() / (1024 * 1024));
-  Serial.printf("Used space: %lluMB\n", SD.usedBytes() / (1024 * 1024));
+
+  // if(!SD.begin(5)){
+  //   Serial.println("Card Mount Failed");
+  //   return;
+  // }
+  // uint8_t cardType = SD.cardType();
+  // Serial.printf("Total space: %lluMB\n", SD.totalBytes() / (1024 * 1024));
+  // Serial.printf("Used space: %lluMB\n", SD.usedBytes() / (1024 * 1024));
+  
   dht.begin();
+
+  if (!rtc.begin()) {
+    Serial.println("Couldnt´t find RTC");
+    while(1);
+  }
+  rtc.adjust(DateTime(F(__DATE__),F(__TIME__)));
+
+
 }
 
 void loop() {
@@ -52,9 +68,9 @@ void loop() {
     Serial.println(F("Failed to read from DHT sensor!"));
     return;
   }
-  // Compute heat index in Celsius (isFahreheit = false)
-  float hic = dht.computeHeatIndex(t, h, false);
-  
-  String writeData = "hydro_greenhouse,position=left temp=" + String(t) + ",hum=" + String(h) + "\n";
-  appendFile(SD, dataloggerID, (writeData).c_str());
+  DateTime now = rtc.now();
+  String time = String(now.unixtime());
+  String writeData = "hydro_greenhouse,position=left temp=" + String(t) + ",hum=" + String(h) + " " + time + "\n";
+  // appendFile(SD, dataloggerID, (writeData).c_str());
+  Serial.print(writeData);
 }
