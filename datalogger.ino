3aa334170c4ea7cae4da6693d1906975dc342b99
hofmn <hofmanndavid92@gmail.com>
2023-07-12 10:07:27 +0200

    Clean up Code

--- datalogger.ino
+++ datalogger.ino
@@ -7,13 +7,15 @@
 #include <RTClib.h>
 
 
-#define dataloggerID "/left.txt"
+#define fileName "/data.txt"
+#define dataloggerID "left"
 #define dhtPin 17
 #define dhtType DHT22
-#define rtcSDA 26
-#define rtcSCL 18
+
 DHT dht(dhtPin, dhtType);
 RTC_DS3231 rtc;
+float t;
+float h;
 
 void appendFile(fs::FS &fs, const char * path, const char * message){
   Serial.printf("Appending to file: %s\n", path);
@@ -31,46 +33,59 @@ void appendFile(fs::FS &fs, const char * path, const char * message){
   file.close();
 }
 
+void sdCardSetup(){
+  if(!SD.begin(5)){
+    Serial.println("Card Mount Failed");
+    return;
+  }
+  uint8_t cardType = SD.cardType();
+  Serial.printf("Total space: %lluMB\n", SD.totalBytes() / (1024 * 1024));
+  Serial.printf("Used space: %lluMB\n", SD.usedBytes() / (1024 * 1024));
+}
+
+void rtcSetup(){
+  if (!rtc.begin()) {
+    Serial.println("Couldnt´t find RTC");
+    appendFile(SD, fileName, "--- Couldn't find RTC --- \n");
+    while(1);
+  }
+  if (rtc.lostPower()) {
+    rtc.adjust(DateTime(F(__DATE__),F(__TIME__)));
+    Serial.println("RTC lost power");
+    appendFile(SD, fileName, "--- RTC LOST POWER --- \n");
+  }
+}
+
+void dhtSetup(){
+  dht.begin();
+  delay(2000);
+  h = dht.readHumidity();
+  t = dht.readTemperature();
+    if (isnan(h) || isnan(t)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    appendFile(SD, fileName, "--- DHT READ FAILED --- \n");
+  }
+}
+
+void writeData(float t, float h, String ID){
+  DateTime now = rtc.now();
+  String time = String(now.unixtime());
+  String writeData = "hydro_greenhouse,position=" + ID + " temp=" + String(t) 
+  + ",hum=" + String(h) + " " + time + "\n";
+  appendFile(SD, fileName, (writeData).c_str());
+  Serial.print(writeData);
+}
 
 void setup(){
   Serial.begin(115200);
-
-  // if(!SD.begin(5)){
-  //   Serial.println("Card Mount Failed");
-  //   return;
-  // }
-  // uint8_t cardType = SD.cardType();
-  // Serial.printf("Total space: %lluMB\n", SD.totalBytes() / (1024 * 1024));
-  // Serial.printf("Used space: %lluMB\n", SD.usedBytes() / (1024 * 1024));
-  
-  dht.begin();
-
-  if (!rtc.begin()) {
-    Serial.println("Couldnt´t find RTC");
-    while(1);
-  }
-  rtc.adjust(DateTime(F(__DATE__),F(__TIME__)));
-
-
+  sdCardSetup();
+  delay(1000);
+  rtcSetup();
+  delay(1000);
+  dhtSetup();
+  writeData(t, h, dataloggerID);
+  esp_deep_sleep(55*1000000);
 }
 
 void loop() {
-  // Wait a few seconds between measurements.
-  delay(2000);
-
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  float h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t)) {
-    Serial.println(F("Failed to read from DHT sensor!"));
-    return;
-  }
-  DateTime now = rtc.now();
-  String time = String(now.unixtime());
-  String writeData = "hydro_greenhouse,position=left temp=" + String(t) + ",hum=" + String(h) + " " + time + "\n";
-  // appendFile(SD, dataloggerID, (writeData).c_str());
-  Serial.print(writeData);
 }
